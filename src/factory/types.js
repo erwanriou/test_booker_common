f3baeefcf5b7e6f6ad9d20e914a1f95df2978b4e
erwanriou <rr.1.riou@gmail.com>
2021-09-30 23:24:43 +0200

    [Library] - update version

--- src/factory/types.js
+++ src/factory/types.js
@@ -5,12 +5,15 @@ const Subject = {
   USER_UPDATED: "user:updated",
   USER_PASSWORD_RESET_REQ: "user:password_reset_req",
   USER_PASSWORD_RESET_RES: "user:password_reset_res",
+  // OFFERS
+  OFFER_CREATED: "offer:created",
+  OFFER_UPDATED: "offer:updated"
 }
 
 // DEFINE ALL LISTENER QUEUE GROUP NAMES
 const QueueGroupName = {
   USER_SERVICE: "user-service",
-
+  OFFER_SERVICE: "offer-service"
 }
 
 exports.Subject = Subject
